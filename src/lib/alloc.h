ec73b4313656efdbc394c48277db928989726cd1
Daniel Kugel <mail4dk@gmail.com>
2015-10-01 22:16:20 +0300

    Split alloc header file.

--- src/lib/alloc.h
+++ src/lib/alloc.h
@@ -2,25 +2,13 @@
 #define _ALLOC_H_
 
 #ifdef __KERNEL__
-#include <linux/vmalloc.h>
+#include <linux/types.h>
 #else
-#include <stdlib.h>
+#include <stddef.h>
 #endif
 
-void *alloc(size_t size) {
-	#ifdef __KERNEL__
-	return vmalloc(size);
-	#else
-	return malloc(size);
-	#endif
-}
+void *alloc(size_t size);
 
-void release(void *ptr) {
-	#ifdef __KERNEL__
-	vfree(ptr);
-	#else
-	free(ptr);
-	#endif
-}
+void release(void *ptr);
 
 #endif
