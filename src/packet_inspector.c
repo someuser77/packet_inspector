d41baef9fd7b2b60da54047b2819ea25d7c3c911
Daniel Kugel <mail4dk@gmail.com>
2015-12-14 16:25:55 +0200

    Packet_inspector to use directional options.

--- src/packet_inspector.c
+++ src/packet_inspector.c
@@ -165,11 +165,13 @@ int main(int __attribute__((unused)) argc, char __attribute__((unused)) *argv[])
 	unsigned char *buffer;
 	size_t size;
 	FilterClient *filterClient;
-	FilterOptions *filterOptions;	
+	DirectionalFilterOptions *options = DirectionalFilterOptions_Create();
+	FilterOptions *incoming, *outgoing;
 	ParserRepository *repository = ParserRepository_Create();
 	
-	printf("PID: %d", getpid());
-	filterOptions = FilterOptions_Create();
+	printf("PID: %d\n", getpid());
+	incoming = FilterOptions_Create();
+	outgoing = FilterOptions_Create();
 	
 	unsigned char srcMac[ETH_ALEN] = {0x6, 0x5, 0x4, 0x3, 0x2, 0x1};
 	unsigned char dstMac[ETH_ALEN] = {0x00, 0x22, 0xfa, 0xe8, 0xc2, 0x42};
@@ -178,29 +180,31 @@ int main(int __attribute__((unused)) argc, char __attribute__((unused)) *argv[])
 	unsigned char srcIp6[IP6_ALEN] = { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf };
 	unsigned char dstIp6[IP6_ALEN] = { 0xf, 0xe, 0xd, 0xc, 0xb, 0xa, 0x9, 0x8, 0x7, 0x6, 0x5, 0x4, 0x3, 0x2, 0x1 };	
 	
-	inet_pton(AF_INET, "192.0.2.33", &srcIp);
-	inet_pton(AF_INET, "192.229.233.146", &dstIp);
+	inet_pton(AF_INET, "10.0.0.5", &srcIp);
+	inet_pton(AF_INET, "10.0.0.5", &dstIp);
 	
-	//filterOptions->setSrcMac(filterOptions, srcMac);
+	incoming->setDevice(incoming, "wlan0", 5);
+	outgoing->setDevice(outgoing, "wlan0", 5);
 	
-	//filterOptions->setDstMac(filterOptions, dstMac);
+	incoming->setProtocol(incoming, IPPROTO_TCP);
+	outgoing->setProtocol(outgoing, IPPROTO_TCP);
 	
-	//filterOptions->setSrcIp(filterOptions, srcIp);
-	//filterOptions->setDstIp(filterOptions, ntohl(dstIp));
+	incoming->setEtherType(incoming, ETH_P_IP);
+	outgoing->setEtherType(outgoing, ETH_P_IP);
 	
-	//filterOptions->setSrcIp6(filterOptions, srcIp6);
-	//filterOptions->setDstIp6(filterOptions, dstIp6);
+	outgoing->setDstPort(outgoing, 80);
 	
-	filterOptions->setDevice(filterOptions, "wlan0", 5);
-	
-	filterOptions->setProtocol(filterOptions, IPPROTO_TCP);
-	
-	filterOptions->setEtherType(filterOptions, ETH_P_IP);
+	incoming->setDstIp(incoming, ntohl(dstIp));
+	outgoing->setSrcIp(outgoing, ntohl(srcIp));
 	
 	//filterOptions->setSrcPort(filterOptions, 80);
 	//filterOptions->setDstPort(filterOptions, 80);
 	
-	printf("Initializing with %s", filterOptions->description(filterOptions));
+	printf("Incoming: %s", incoming->description(incoming));
+	printf("Outgoing: %s", outgoing->description(outgoing));
+	
+	options->setIncomingFilterOptions(options, incoming);
+	options->setOutgoingFilterOptions(options, outgoing);
 	
 	filterClient = FilterClient_Create();
 	
@@ -209,7 +213,7 @@ int main(int __attribute__((unused)) argc, char __attribute__((unused)) *argv[])
 		return EXIT_FAILURE;
 	}
 	
-	if (!filterClient->initialize(filterClient, filterOptions)) {
+	if (!filterClient->initialize(filterClient, options)) {
 		printf("Error initializing. Did you remember to load the module?\n");
 		return EXIT_FAILURE;
 	}
@@ -218,7 +222,7 @@ int main(int __attribute__((unused)) argc, char __attribute__((unused)) *argv[])
 		printf("Waiting for data... \n");
 		fflush(stdout);
 		buffer = filterClient->receive(filterClient, &size);
-		if (buffer == NULL) {
+		if (!buffer) {
 			break;
 		}
 		printf("==========[ %zu bytes ]===============\n", size);
